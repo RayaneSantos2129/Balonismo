362ac93beb1838c55087aea9534b875f1cf82be8
Rayane Vieira <rayvieiraaa@gmail.com>
2023-07-27 10:14:42 -0300

    atividade legal, finalizando projeto.

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ public class Main {
         // construindo balões
         Balao b1 = new Balao();
 
-        //chamado o método voar()
-        String resultado = b1.voar (new combustivel());
-
-        // exibindo o resultado do voo
-        System.out.println(resultado);
-
         Balao b2 = new Balao(10, "rosa", "grande", 1.43, 23.65, "grande", 4 );
         Balao b3 = new Balao(12, "verde", "pequeno", 1.21, 78.92, "medio", 12);
-        
+
+        // construindo pessoa
+        Pessoa p1 = new Pessoa("Rayane", "7438328", 795345, 55119433, 551193432, "rua. arco-iris");
+        Pessoa p2 = new Pessoa("Ane", "7653456", 439023, 55110349, 551190549, "rua. gramado");
+
+        // construindo piloto
+        Piloto pi1 = new Piloto("Ronaldo", "743829", 842394);
+
+
 
     }
 }
